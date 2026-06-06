f376347f35d00145a18ff2ee4653b97850971777
ekta-dew25 <ekta.dewangan1995@gmail.com>
2026-06-06 16:23:46 +0530

    redis done

---
+++ src/utils/v1/cache.js
@@ -0,0 +1,23 @@
+// import { redisClient } from "../config/redis.js";
+
+import { redisClient } from "../../config/redis.js";
+
+export const getCache = async (key) => {
+  const data = await redisClient.get(key);
+
+  return data ? JSON.parse(data) : null;
+};
+
+export const setCache = async (key, value, ttl = 600) => {
+  await redisClient.set(key, JSON.stringify(value), "EX", ttl);
+};
+
+export const deleteCache = async (key) => {
+  await redisClient.del(key);
+};
+
+export const redisKeys = {
+  organizations: () => "org:list",
+
+  organizationById: (orgId) => `org:${orgId}`,
+};
